72ee6e7537cc4b3561da849749e31a4ae2ad0046
bdamarjati <bimadamar@student.uns.ac.id>
2024-12-27 10:31:18 +0700

    add jwt token maker

---
+++ token/payload.go
@@ -0,0 +1,18 @@
+package token
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token is expired")
+)
+
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
